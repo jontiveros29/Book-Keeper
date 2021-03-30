931d2dbb1aabc5ec673886798e2d9e78b6650fe4
Jose Ontiveros <jontiveros0829@gmail.com>
2021-03-29 21:43:17 -0400

    Notes Keeper App

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FlareIcon from "@material-ui/icons/Flare";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FlareIcon />
+        Life Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+    {
+      key: 1,
+      title: "The Obstacle Is The Way",
+      content:
+        "Think progress, not perfection."
+    },
+    {
+      key: 2,
+      title: "The Subtle Art of Not Giving a F",
+      content:
+        "To be happy we need something to solve. Happiness is therefore a form of action"
+    },
+    {
+      key: 3,
+      title: "Mindset",
+      content:
+        "No matter what your ability is, effort is what ignites that ability and turns it into accomplishment."
+    },
+    {
+      key: 4,
+      title: "Rich Dad Poor Dad",
+      content:
+        "If fear is too strong, the genius is suppressed."
+    }
+  ];
+
+  export default notes;
